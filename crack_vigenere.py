0fa4648c770bb59cd29e6b81bebabe6f32a846da
rico leone <ricoleone@users.noreply.github.com>
2023-02-13 11:58:18 +0000

    improvements and new examples

--- crack_vigenere.py
+++ crack_vigenere.py
@@ -1,3 +1,11 @@
 f = open('encrypted.txt','r')
 words = [word.strip() for word in f]
 f.close()
+
+onlyletters = ''.join(filter(lambda x: x.isalpha(), words))
+loweronly = onlyletters.lower()
+# we know the key is length 10 so lets look at every 1oth character and cycle through the alphabet until we get the expected sum of the squares of frequncies for the 10 char key
+for i in range(10):
+  frequency = {}
+  for ascii in range(ord('a'), ord('a')+26):
+    frequency[chr(ascii)] = float(loweronly.count(chr(ascii)))/len(loweronly)
