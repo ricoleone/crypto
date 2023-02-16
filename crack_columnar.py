0aef4bad00221b6164c9af61a715f8fcbaf8b8dd
rico leone <ricoleone@users.noreply.github.com>
2023-02-16 23:12:06 +0000

    updated columnar

--- crack_columnar.py
+++ crack_columnar.py
@@ -1,8 +1,42 @@
-#######################################################################################
-# Name: encrypt
-#######################################################################################
 from itertools import permutations
-import time
+import time, random
+
+# column encryt from Andy
+def col_trans(plain):
+    cols = int(9) #random.randint(8,10)
+    key = range(cols)
+    key = random.sample(key, k=len(key))
+    #print("KEY TYPE", type(key))
+    return "".join(plain[i::cols].upper() for i in key), key
+
+
+#######################################################################################
+# col_decrypt(ciphertext, key)
+######################################################################################
+def col_decrypt(cipher, key):
+  cols = len(key)
+  rows = int(len(cipher)/cols)
+  #separate cipher into coluns
+  columns = [cipher[i*rows:i*rows + rows ] for i in range(cols)]
+  #reorder columns back to the original order
+  tmp = [''] * cols
+  j = 0
+  for i in key:
+    tmp[i] = columns[j]
+    j +=1
+  columns = tmp
+  #columns = [insert(key, x) ]
+  #create the plain text by reading across rows
+  # read across each row to get the plaintext
+  pt = ""
+  for row in range(rows):
+    for column in range(cols):
+      pt += columns[column][row]
+  return pt
+
+#######################################################################################
+# Name: encryp
+#######################################################################################
 
 def encrypt(plaintext, key):
     ciphertext = [''] * len(key)
@@ -26,6 +60,8 @@ def encrypt(plaintext, key):
 # Name: decrypt
 #######################################################################################
 def decrypt(ct, key):
+  print("key in decrypt ", key)
+  #print("key type ", type(key))
   keysize = len(key)
   blocksize = int(len(ct)/keysize) 
   ciphertext = [''] * keysize
@@ -33,20 +69,22 @@ def decrypt(ct, key):
   for block in range(keysize):
     ciphertext[block] = ct[block * blocksize: block * blocksize + blocksize ]
   
-  #shuffle column order from alphabetical to the order of the key [word]
+  #shuffle columns to be realigned with the key order
   alphaOrder = ''.join(sorted(key))
+  print("alphaOreder =", alphaOrder)
   finalciphertext = [''] * len(key)
   for i in range(len(key)):
     for j in range(len(key)):
       if key[i] == alphaOrder[j]:
         finalciphertext[i] = ciphertext[j]
-  # read across each column to get the plaintext
+        
+  # read across each row to get the plaintext
   plaintext = ""
   for row in range(blocksize):
     for column in range(keysize):
       plaintext += finalciphertext[column][row]
-      #print(ciphertext)
   return plaintext
+
 
 ####################################################################################
 # Name: fitness
@@ -54,7 +92,7 @@ def decrypt(ct, key):
 ####################################################################################
 def fitness(plaintext):
   ptlen = len(plaintext)
-  words = ["THE", "MAN", "MEN", "AND", "ING", "ENT", "ION", "HER", "FOR", "ONE", "THIS", "FIGHT", "COUNTRY", "LIBERTY", "DEATH"]
+  words = ["THE", "MAN", "MEN", "AND", "ING", "ENT", "ION", "HER", "FOR", "ONE", "THIS", "FIGHT", "COUNTRY", "LIBERTY", "DEATH", "LIVE"]
   score = 0
   for word in words:
     wordlen = len(word)
@@ -68,21 +106,26 @@ def fitness(plaintext):
 # Name: test
 # Description: test the columnar decrypt and encrypt functions
 ####################################################################################
-def test(plaintext = "THISISASHORTBLOCKOFTEXTTOSEEIFIGOTITRIGHTSNOWFOOBA", key = "4329756180", verbose = True):
+def test(plaintext = "THISISASHORTBLOCKOFTEXTTOSEEIFIGOTITRIGHTSNOWFOOBA", key = "42031"):
   
-  ct = encrypt(plaintext, key)
-  pt = decrypt(ct, key)
-
-  if verbose:
-    print("plaintext = ", plaintext)
-    print("ct =        ", ct)
-    print("pt =        ", pt)
-    print("fitness score = ",fitness(pt))
+  ct, ck = col_trans(plaintext)
+  #print("ct = ", ct)
+  #print("ck key =", ck)
+  #print("ck type ", type(ck))
+  temp = ""
+  for i in ck:
+    temp += chr(i + ord('0'))
+  print("tmp= ", temp)
+  key = temp
+  pt = col_decrypt(ct, ck)
+  print("plaintext = ", plaintext)
+  print("ct =        ", ct)
+  print("pt =        ", pt)
+  print("fitness score = ",fitness(pt))
   if plaintext == pt:
     return True
   return False
 
-  
 ####################################################################################
 # Name: solve
 # Description: Solve the Black Hat Challenge for Columnar encoded text with no
@@ -94,28 +137,41 @@ def test(plaintext = "THISISASHORTBLOCKOFTEXTTOSEEIFIGOTITRIGHTSNOWFOOBA", key =
 #              the file "mostfit.txt" in the current directory
 ####################################################################################
 def solve(ciphertext):
-  keys = permutations("0123456789")
+  keys = permutations(range(9))
   bestkey = ""
   highscore = 0
-  for i in list(keys):
-    key =  "".join(i)
-    pt = decrypt(ciphertext, key)
+  solved = ""
+  for key in list(keys):
+    #key =  "".join(i)
+    pt = col_decrypt(ciphertext, key)
     score = fitness(pt)
     if score > highscore:
       highscore = score
       bestkey = key
+      solved  = pt
       
-  print("high score: ", highscore, "key: ", bestkey)  
+  print("high score: ", highscore, "key: ", bestkey)
+  print(solved)
   
 
-print("testing solve")
+#print("testing solve")
 
-pt = "THISISASHORTBLOCKOFTEXTTOSEEIFIGOTITRIGHTSNOWFOOBA"
-key = "5964203178"
-ct = encrypt(pt, key)
+#pt = "THISISASHORTBLOCKOFTEXTTOSEEIFIGOTITRIGHTSNOWFOOBARLIBERTYME"
+pt = "nxhvbvkqyfxgzzmrgkgjqwrqrqdizzrcrpublrizptbvrckgsunalszixdbvgdquagbeqqhm"
+pt = pt.upper()
+print("plaint text lenght = ", len(pt) )
+#key = "6140873952"
+ct, ck = col_trans(pt)
 starttime = time.time()
 solve(ct)
 endtime = time.time()
 print("elapsed time: ", endtime - starttime)
+print("pt      = ", pt)
+print("ct      = ", ct)
+print("ck      = ", ck)
+
 #print("running test")
+#starttime= time.time()
 #test()
+#endtime = time.time()
+#print("elapsed time: ", endtime - starttime)
